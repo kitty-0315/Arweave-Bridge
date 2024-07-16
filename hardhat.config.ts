cd4172312f9275bc543f7c511cce59ca38861f21
kitty-0315 <tarispltok.tp@gmail.com>
2024-07-16 22:23:32 +0200

    fix: add hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const private_key = process.env.WALLET_PHARSE || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
-    
+    ethereum: {
+      url: 'https://rpc.sepolia.org',
+      accounts: [private_key]
+    }
   }
 };
 
